c376afb1f2e109f9cb8b0181bbfde39542a370df
emma-landry7 <emmablandry730@gmail.com>
2023-10-21 23:16:18 -0500

    update question 3 of JS intermediate lab. to be continued

--- intermediate.js
+++ intermediate.js
@@ -7,7 +7,7 @@ function ucFirstLetters(string) {
     return separate.join(' ');
 }
 
-console.log(ucFirstLetters("los angeles")) //Los Angeles
+// console.log(ucFirstLetters("los angeles")) //Los Angeles
 
 // function truncate(str, max) {
 //     if (str.length <= max) {
@@ -23,7 +23,27 @@ const truncate = function(str, max) {
     return str.slice(0, max) + "..."
 }
 
-console.log(truncate('This text will be truncated if it is too long', 25)) //This text will be truncat...
-console.log(truncate("This is a test of my declaration function", 15))
-console.log(truncate('Test 2 of my declaration function', 50))
+// console.log(truncate('This text will be truncated if it is too long', 25)) //This text will be truncat...
+// console.log(truncate("This is a test of my declaration function", 15))
+// console.log(truncate('Test 2 of my declaration function', 50))
 
+const animals = ['Tiger', 'Giraffe']
+animals.push('Bear')
+animals.push('Hippo')
+animals.unshift('Lion')
+animals.unshift('Monkey')
+animals.push('Alligator')
+animals.sort()
+
+function replaceMiddleAnimal(newValue) {
+    animals.splice(3, 1, newValue);
+    return animals;
+}
+
+console.log(animals)
+console.log(replaceMiddleAnimal('Bat'))
+
+// function findMatchingAnimals(beginsWith) {
+    let beginsWith = animals.filter(start => start.startsWith('B'))
+// }
+console.log(beginsWith)
