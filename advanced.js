c176e2440e9839f3213f02c6afcf6ba4907a3901
emma-landry7 <emmablandry730@gmail.com>
2023-11-04 14:53:22 -0500

    completed question 1 of advanced lab

--- advanced.js
+++ advanced.js
@@ -1,9 +1,20 @@
-let counter1 = makeCounter(0);
+// Question 1:
+function makeCounter(startsFrom, incrementBy) {
+    let currentCount = startsFrom;
+
+    return function() {
+        currentCount += incrementBy;
+        console.log(currentCount)
+        return currentCount;
+    };
+}
+
+let counter1 = makeCounter(0, 1);
 
 counter1()
 counter1()
 
-let counter2 = makeCounter(7)
+let counter2 = makeCounter(5, 5)
 counter2()
 counter2()
 counter2()
